6d28d1c78a96c86710c41555a5a53668069e7341
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-12-11 15:21:57 +0100

    Adds FormSkuFormBuilderComponent

---
+++ src/app/form-sku-form-builder/form-sku-form-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-sku-form-builder',
+  templateUrl: './form-sku-form-builder.component.html',
+  styleUrls: ['./form-sku-form-builder.component.css']
+})
+export class FormSkuFormBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
