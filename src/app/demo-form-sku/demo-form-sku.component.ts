0f78c95c8d208a39c97d23b55373540f4f7838ce
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-12-11 15:15:29 +0100

    Implements DemoFormSkuComponent

--- src/app/demo-form-sku/demo-form-sku.component.ts
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'demo-form-sku',
   templateUrl: './demo-form-sku.component.html',
   styleUrls: ['./demo-form-sku.component.css']
 })
-export class DemoFormSkuComponent implements OnInit {
+export class DemoFormSkuComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  onSubmit(form: any): void {
+    console.log('you submitted value', form);
   }
-
 }
